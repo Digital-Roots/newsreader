78657195cb2fe324420d1ec141bc8f56fd656d1a
Robert Edwardes <rdedwardes@gmail.com>
2017-09-16 21:02:17 -0400

    add missing mark-up

--- js/ko.js
+++ js/ko.js
@@ -3,9 +3,9 @@
 $.getJSON("https://newsapi.org/v1/articles?source=breitbart-news&sortBy=top&apiKey=9d2c4463caef4233bf44d61aa88066f8", function(data){
   var set = JSON.parse(data);
   var viewModel = {
-    Title : ko.observableArray([set.article.title])
-    Aurthor: ko.observableArray([set.article.author])
-    PublishedAt: ko.observableArray([set.article.publishedAt])
-    url: ko.obervableArray([set.arrticle.url])
+    Title : ko.observableArray([set.article.title]),
+    Aurthor: ko.observableArray([set.article.author]),
+    PublishedAt: ko.observableArray([set.article.publishedAt]),
+    url: ko.obervableArray([set.arrticle.url]),
   };
 };)
