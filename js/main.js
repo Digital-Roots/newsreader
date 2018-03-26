e330d0ae61e539102807ab8e7a9a045b2f0e6ea7
Robert Edwardes <rdedwardes@gmail.com>
2018-03-26 14:04:14 -0400

    aria

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   function getNews(){
-    let yqlPoliti ="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20(url%3D'http%3A%2F%2Fwww.politico.com%2Frss%2Fpoliticopicks.xml')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    const yqlPoliti ="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20(url%3D'http%3A%2F%2Fwww.politico.com%2Frss%2Fpoliticopicks.xml')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
     $.getJSON(yqlPoliti, function(Politi) {
       const res = Politi.query.results.item;
       res.forEach(function(x, y){
@@ -14,7 +14,7 @@ $(document).ready(function() {
         }
       });
     }, "jsonp");
-    let yqlHill = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20(url%3D'http%3A%2F%2Fthehill.com%2Ftaxonomy%2Fterm%2F1077%2Ffeed')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    const yqlHill = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20(url%3D'http%3A%2F%2Fthehill.com%2Ftaxonomy%2Fterm%2F1077%2Ffeed')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
     $.getJSON(yqlHill, function(Hill) {
       const res = Hill.query.results.item;
       res.forEach(function(x, y){
@@ -28,7 +28,7 @@ $(document).ready(function() {
         }
       });
     }, "jsonp");
-    let yqlWp =  "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20(url%3D'http%3A%2F%2Ffeeds.washingtonpost.com%2Frss%2Fpolitics')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    const yqlWp =  "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20(url%3D'http%3A%2F%2Ffeeds.washingtonpost.com%2Frss%2Fpolitics')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
     $.getJSON(yqlWp, function(Wp) {
       const res = Wp.query.results.item;
       res.forEach(function(x, y){
