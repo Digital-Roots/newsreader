2e082e70a1e5366eec2375f12d5c9248fcec06b0
Robert Edwardes <rdedwardes@gmail.com>
2018-09-28 19:02:05 -0400

    input need to fix html loop so rss goes into single column

--- js/main.js
+++ js/main.js
@@ -7,65 +7,38 @@ function openStream(stream){
 };
 window.onload = openStream(openBloomberg);
 $(document).ready(function() {
-  function getNews(){
-    const yqlPoliti ="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20(url%3D'http%3A%2F%2Fwww.politico.com%2Frss%2Fpoliticopicks.xml')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-    $.getJSON(yqlPoliti, function(politi) {
-      const res = politi.query.results.item;
+
+
+
+  const rssSubmitButton = document.getElementById('rss-submit');
+  const rssInput = document.getElementById('rss-input');
+  const rssArray = [];
+  const yqlFront = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20(url%3D'";
+  const yqlBack = "')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+
+
+  $('#rss-submit').click(function(){
+    let rssURL = rssInput.value;
+    rssURL = rssURL.replace(/:/g, "%3A").replace(/\//g, "%2F");
+    rssURL = yqlFront + rssURL + yqlBack;
+    rssArray.push(rssURL);
+    console.log(rssArray);
+    rssURL = rssArray;
+    let rssLoop = $.getJSON(rssURL, function(data) {
+      const res = data.query.results.item;
       res.forEach(function(x, y){
         let link = res[y].link;
         let title = res[y].title;
         let description = res[y].description;
         if(description !== null){
-          $("#politi").append("<h4><a target=\"_blank\" rel=\"nofollow\" href=\"" + link + "\">" + title + "</a></h4><p>" + description + "</p>");
+          $('#rss-feed').append("<section role='feed'><header><h2>New Place Holder</h2></header><article aria-live='polite' tabindex='1'><h4><a target=\"_blank\" rel=\"nofollow\" href=\"" + link + "\">" + title + "</a></h4><p>" + description + "</p></article></section>");
         }else{
-          $("#politi").append("<h4><a target=\"_blank\" rel=\" nofollow\"href=\"  " + link + "  \">" + title + "</a></h4><p> No summary given</p>");
+          $('#rss-feed').append("<section role='feed'><header><h2>New Place Holder</h2></header><article aria-live='polite' tabindex='1'><h4><a target=\"_blank\" rel=\"nofollow\" href=\"" + link + "  \">" + title + "</a></h4><p> No summary given</p></article></section>");
         }
       });
-    }, "jsonp");
-    const yqlHill = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20(url%3D'http%3A%2F%2Fthehill.com%2Ftaxonomy%2Fterm%2F1077%2Ffeed')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-    $.getJSON(yqlHill, function(hill) {
-      const res = hill.query.results.item;
-      res.forEach(function(x, y){
-        let link = res[y].link;
-        let title = res[y].title;
-        let description = res[y].description;
-        if(description !== null){
-          $("#hill").append("<h4><a target=\"_blank\" rel=\" nofollow\" href=\" " + link + "  \">" + title + "</a></h4><p>" + description + "</p>");
-        }else{
-          $("#hill").append("<h4><a target=\"_blank\" rel=\" nofollow\" href=\"  " + link + "  \">" + title + "</a></h4><p> No summary given</p>");
-        }
-      });
-    }, "jsonp");
-    const yqlWp =  "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20(url%3D'http%3A%2F%2Ffeeds.washingtonpost.com%2Frss%2Fpolitics')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-    $.getJSON(yqlWp, function(wp) {
-      const res = wp.query.results.item;
-      res.forEach(function(x, y){
-        let link = res[y].link;
-        let title = res[y].title;
-        let description = res[y].description;
-        if(description !== null){
-          $("#wp").append("<h4><a target=\"_blank\" rel=\" nofollow\"href=\"  " + link + "  \">" + title + "</a></h4><p>" + description + "</p>");
-        }else{
-          $("#wp").append("<h4><a target=\"_blank\" rel=\" nofollow\"href=\"  " + link + "  \">" + title + "</a></h4><p> No summary given</p>");
-        }
-      });
-    }, "jsonp");
-    const yplNyt = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20(url%20%3D%20'http%3A%2F%2Fwww.nytimes.com%2Fservices%2Fxml%2Frss%2Fnyt%2Fpolitics.xml')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-    $.getJSON(yplNyt, function(nyt) {
-      const res = nyt.query.results.item;
-      res.forEach(function(x, y){
-        let link = res[y].link;
-        let title = res[y].title;
-        let description = res[y].description;
-        if(description !== null){
-          $("#nyt").append("<h4><a target=\"_blank\" rel=\" nofollow\"href=\"  " + link + "  \">" + title + "</a></h4><p>" + description + "</p>");
-        }else{
-          $("#nyt").append("<h4><a target=\"_blank\" rel=\" nofollow\"href=\"  " + link + "  \">" + title + "</a></h4><p> No summary given</p>");
-        }
-      });
-    }, "jsonp");
-    console.log("got news");
-  };
-  window.onload = getNews;
-  setInterval(getNews, 420000);
+    });
+    return rssLoop;
+  });
+
 });
